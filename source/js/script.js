191b0d17076b2f6599c07a12fbce5a8f1ac3b93a
Denys Mogyliuk <denysmogyliuk@gmail.com>
2022-01-15 19:10:15 +0200

    add js for open modal

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,29 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var modalOverlay = document.querySelector(".modal__wrapper");
+var modalCart = modalOverlay.querySelector(".modal");
+var modalButtons = document.querySelectorAll(".modal-button");
+var modalButtonArray = Array.prototype.slice.call(modalButtons);
+
+if (document.querySelector(".modal__wrapper")) {
+  modalButtonArray.forEach(function (el) {
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      modalOverlay.classList.remove("visually-hidden");
+      modalOverlay.classList.add("modal__wrapper--open");
+      modalCart.classList.add("modal--open");
+      document.body.style.overflow = "hidden";
+      if (modalCart.classList.contains("modal--open")) {
+        window.addEventListener("click", function (e) {
+          if (e.target === modalOverlay) {
+            document.body.style.overflow = "visible";
+            modalOverlay.classList.remove("modal__wrapper--open");
+            modalCart.classList.remove("modal--open");
+          }
+        });
+      }
+    });
+  });
+}
