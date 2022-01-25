dded4e80fd838c322c9fb59acbeff06847dd2f73
Denys Mogyliuk <denysmogyliuk@gmail.com>
2022-01-25 13:41:21 +0200

    small fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// Открытие мобильного меню
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -13,30 +15,40 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-var modalOverlay = document.querySelector(".modal__wrapper");
-var modalCart = modalOverlay.querySelector(".modal");
-var modalButtons = document.querySelectorAll(".modal-button");
+// Открытие модального окна
+
+var modalOverlay = document.querySelector('.modal');
+var modalCard = document.querySelector('.modal__card');
+var modalButtons = document.querySelectorAll('.modal-button');
 var modalButtonArray = Array.prototype.slice.call(modalButtons);
 
-if (document.querySelector(".modal__wrapper")) {
+if (document.querySelector('.modal')) {
   modalButtonArray.forEach(function (el) {
-    el.addEventListener("click", function (e) {
+    el.addEventListener('click', function (e) {
       e.preventDefault();
-      modalOverlay.classList.remove("visually-hidden");
-      modalOverlay.classList.add("modal__wrapper--open");
-      modalCart.classList.add("modal--open");
-      document.body.style.overflow = "hidden";
-      document.body.style.padding = "0 15px 0 0";
-      if (modalCart.classList.contains("modal--open")) {
-        window.addEventListener("click", function (e) {
+      modalOverlay.classList.remove('visually-hidden');
+      modalOverlay.classList.add('modal--open');
+      modalCard.classList.add('modal__card--open');
+      document.body.style.overflow = 'hidden';
+      document.body.style.padding = '0 15px 0 0';
+      if (modalCard.classList.contains('modal__card--open')) {
+        window.addEventListener('click', function (e) {
           if (e.target === modalOverlay) {
-            document.body.style.overflow = "visible";
-            document.body.style.padding = "0";
-            modalOverlay.classList.remove("modal__wrapper--open");
-            modalCart.classList.remove("modal--open");
+            document.body.style.overflow = 'visible';
+            document.body.style.padding = '0';
+            modalOverlay.classList.remove('modal--open');
+            modalCard.classList.remove('modal__card--open');
           }
         });
       }
     });
   });
 }
+
+// Управление картой
+
+var mapPicture = document.querySelector('.contacts__map');
+var mapGoogle = document.querySelector('.contacts__google-map');
+
+mapPicture.classList.add('visually-hidden');
+mapGoogle.classList.remove('visually-hidden');
